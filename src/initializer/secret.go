32895cd1123c4f0207693e46957d066ddb32dc05
Krit <tanakritkhunkhae@gmail.com>
2024-03-10 16:09:59 +0700

    try use renderer

--- src/initializer/secret.go
+++ src/initializer/secret.go
@@ -1,8 +1,6 @@
 package initializer
 
 import (
-	secretmanager "cloud.google.com/go/secretmanager/apiv1"
-	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -13,24 +11,38 @@ func GetSecret(ctx context.Context) (*Credential, error) {
 
 	fmt.Println("GET CREDENTIAL")
 
-	client, err := secretmanager.NewClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create Secret Manager client: %v", err)
-	}
-	defer client.Close()
+	//client, err := secretmanager.NewClient(ctx)
+	//if err != nil {
+	//	return nil, fmt.Errorf("failed to create Secret Manager client: %v", err)
+	//}
+	//defer client.Close()
+	//
+	//req := &secretmanagerpb.AccessSecretVersionRequest{
+	//	Name: os.Getenv("PROJECT_ID"),
+	//}
+	//
+	//result, err := client.AccessSecretVersion(ctx, req)
+	//if err != nil {
+	//	return nil, fmt.Errorf("failed to access secret version: %v", err)
+	//}
+	//
+	//var credential Credential
+	//if err := json.Unmarshal(result.Payload.Data, &credential); err != nil {
+	//	return nil, fmt.Errorf("failed to decode secret value: %v", err)
+	//}
 
-	req := &secretmanagerpb.AccessSecretVersionRequest{
-		Name: os.Getenv("PROJECT_ID"),
-	}
-
-	result, err := client.AccessSecretVersion(ctx, req)
+	file, err := os.Open("/etc/secrets/postgres.json")
 	if err != nil {
-		return nil, fmt.Errorf("failed to access secret version: %v", err)
+		fmt.Println("Error opening config file: /etc/secrets/postgres.json", err)
+		return nil, err
 	}
+	defer file.Close()
 
 	var credential Credential
-	if err := json.Unmarshal(result.Payload.Data, &credential); err != nil {
-		return nil, fmt.Errorf("failed to decode secret value: %v", err)
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&credential); err != nil {
+		fmt.Println("Error decoding config file:", err)
+		return nil, err
 	}
 
 	return &credential, nil
